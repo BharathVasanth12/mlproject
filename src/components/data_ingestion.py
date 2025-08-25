7e43294323018880a8fe0e7c6f2076bf65020225
BharathVasanth12 <bharath.vasanthkumar@gmail.com>
2025-08-25 21:17:47 +0530

    data transformation config

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,7 @@ import pandas as pd
 import numpy as np
 from dataclasses import dataclass
 from sklearn.model_selection import train_test_split
+from src.components.data_transformation import DataTransformation, DataTransformationConfig
 
 @dataclass
 class DataIngestionConfig:
@@ -47,4 +48,7 @@ class DataIngestion:
 
 if __name__ == '__main__':
     obj = DataIngestion()
-    obj.initate_data_ingestion()
+    train_data, test_data = obj.initate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data, test_data)
--- src/exception.py
+++ src/exception.py
@@ -12,7 +12,7 @@ class CustomException:
 
     def __init__(self, error_message, error_details:sys):
         super.__init__(error_message)
-        self.error_message = error_message_detail(error_message, error_details=error_details)
+        self.error_message = error_message_details(error_message, error_details=error_details)
 
     def __str__(self):
         return self.error_message
